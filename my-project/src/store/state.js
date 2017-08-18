789c0928fddc6cf98beb929eb739523ce7b8ee87
ztiantian <243752773@qq.com>
2017-08-18 14:32:48 +0800

    8/18

---
+++ my-project/src/store/state.js
@@ -0,0 +1,17 @@
+const moduleA = {
+  state: {
+    a: 1,
+    b: 2,
+    count: 1
+  },
+  mutations: {increment (state) { state.count++ }
+  },
+  actions: {},
+  getters: {
+    doubleCount: (state) => {
+      return state.a * 2
+    }
+  }
+}
+
+export default moduleA
